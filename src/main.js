fd4371d0876b10dd0839da144fb0922c97132763
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-23 17:40:39 +0000

    prototyping

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { ARTLIST } from "./global_artlist.js";
 
-var p = document.createElement("p")
-p.innerHTML = ARTLIST[0]
+var p = document.createElement("img")
+p.src = ARTLIST[0].src + ARTLIST[0].ext
+p.width = ARTLIST[0].dimensions[0]
+p.height = ARTLIST[0].dimensions[1]
 document.body.append(p)
